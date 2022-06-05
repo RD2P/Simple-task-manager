10aedfed1082f077733596c5243645e1897f5963
Raphael De Los Reyes <graphael.dr@gmail.com>
2022-06-05 11:28:54 -0600

    update

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-//https://www.youtube.com/watch?v=DqaTKBU9TZk
-//47.57
-
 let Tasks = [{
         name:'eat',
         dueDate:'2022-06-10'
@@ -28,9 +25,14 @@ function display(){
 display(); //render tasks from array on top
 
 function addTask(){
-    let textbox = document.getElementById('newTaskIn');
-    let newTask = textbox.value;
-    Tasks.push(newTask);
+    const textbox = document.getElementById('newTaskIn');
+    const newTask = textbox.value;
+    const dateSelector = document.getElementById('date-selector');
+    const dueDate = dateSelector.value;
+    Tasks.push({
+        name:newTask,
+        dueDate:dueDate
+    });
     display();
 }
 
@@ -47,82 +49,3 @@ function taskDone(){
 
 
 
-/* CM TO INCHES CONVERTER************
-function toCM(){
-    let box = document.getElementById('number-input');
-    let input = box.value;
-    let ans = input*2.54;
-    let d = document.createElement('div');
-    d.innerText = ans + ' cm';
-    let answer = document.getElementById('result');
-    answer.appendChild(d);
-}
-function toINCH(){
-    let box = document.getElementById('number-input');
-    let input = box.value;
-    let ans = input/2.54;
-    let d = document.createElement('div');
-    d.innerText = ans + ' inches';
-    let answer = document.getElementById('result');
-    answer.appendChild(d);
-} */
-
-
-/* Shopping cart practice *************
-function addItem(item){
-    let groceryList = document.getElementById('cart');
-    let f = document.createElement('div');
-    f.innerText = item;
-    groceryList.appendChild(f);
-}
-
-function clearCart(){
-    document.getElementById('cart').innerHTML = '';
-} */
-
-
-/* Sum all numbers in array *****************
-let sum = 0;
-function arraySum(numArr){
-    
-    numArr.forEach(function(num){
-        sum = sum+num;
-    });
-}
-
-arraySum([2,3,5,6,7,8]);
-console.log(sum);
-*/
-
-/*
-let trees = ['bigTree', 'greenTree'];
-trees.push('wildTree');
-trees.forEach(function(tree){
-    console.log(tree);
-})*/
-
-/*
-let sum = 0;
-function arraySum(arr){
-    arr.forEach(function(num){
-        sum += num;
-    })
-    console.log(sum);
-}
-
-arraySum([2,3,4]); 
-
-/* Two buttons which increase or decrease a number***********
-let count = 0;
-
-function addCount(){
-    count = count + 1;
-    let counter = document.getElementById('counter');
-    counter.innerText = count;
-}
-
-function subtractCount(){
-    count -= 1;
-    let counter = document.getElementById('counter');
-    counter.innerText = count;
-} */
